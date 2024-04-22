a57347637172393d19988ae409272a88cd8d132f
victorpreto10 <150963273+victorpreto10@users.noreply.github.com>
2024-04-22 18:17:31 -0300

    Update teste5.py

--- teste5.py
+++ teste5.py
@@ -515,46 +515,33 @@ if 'limpar_adicionais' not in st.session_state:
     st.session_state['limpar_adicionais'] = False
 
 elif opcao == 'Niveis Kapitalo':
-
-with st.sidebar:
-    st.header("Inserir e Gerenciar Dados")
-    if not st.session_state['dados_operacoes']:
-        dados_raw = st.text_area("Cole os dados das operações aqui:", height=150, key="dados_iniciais", value="")
+    with st.sidebar:
+        st.header("Inserir e Gerenciar Dados")
+        
+        dados_raw = st.text_area("Cole os dados das operações aqui:", height=150, key="dados_iniciais")
         if st.button("Salvar Dados Iniciais"):
             if dados_raw:
                 linhas = dados_raw.strip().split("\n")
                 st.session_state['dados_operacoes'].extend(linhas)
                 st.success("Dados adicionados com sucesso!")
 
-        # Botão para adicionar operações adicionais
-    dados_adicionais = st.text_area("Cole operações adicionais aqui:", height=150, key="dados_adicionais",
-                                    value=st.session_state.get('dados_adicionais', ''))
-    if st.button("Adicionar Operações"):
-        if dados_adicionais:
-            linhas_adicionais = dados_adicionais.strip().split("\n")
-            st.session_state['dados_operacoes'].extend(linhas_adicionais)
-            st.success("Operações adicionais adicionadas com sucesso!")
-            st.session_state['limpar_adicionais'] = True
+        dados_adicionais = st.text_area("Cole operações adicionais aqui:", height=150, key="dados_adicionais")
+        if st.button("Adicionar Operações"):
+            if dados_adicionais:
+                linhas_adicionais = dados_adicionais.strip().split("\n")
+                st.session_state['dados_operacoes'].extend(linhas_adicionais)
+                st.success("Operações adicionais adicionadas com sucesso!")
 
-    # Botão para apagar todos os dados
-    if st.button("Apagar Todos os Dados"):
-        st.session_state['dados_operacoes'] = []
-        st.session_state['px_ref_por_ativo'] = {}
-        st.session_state['limpar_adicionais'] = False
-        st.experimental_rerun()
+        if st.button("Apagar Todos os Dados"):
+            st.session_state['dados_operacoes'] = []
+            st.experimental_rerun()
 
-# Exibição das operações e interação com o usuário para seleção de ticker e Px Ref.
-if st.session_state['dados_operacoes']:
     operacoes_processadas = processar_dados(st.session_state['dados_operacoes'])
     tickers = list(operacoes_processadas.keys())
     ticker_escolhido = st.selectbox("Escolha um ticker:", [""] + tickers)
     if ticker_escolhido:
-        px_ref_key = f"px_ref_{ticker_escolhido}"
-        px_ref = st.number_input("Px Ref.:", min_value=0.01, step=0.01, format="%.2f", key=px_ref_key,
-                                 value=st.session_state['px_ref_por_ativo'].get(ticker_escolhido, 0.01))
-        st.session_state['px_ref_por_ativo'][ticker_escolhido] = px_ref
+        px_ref = st.number_input("Px Ref.:", min_value=0.01, step=0.01, format="%.2f", key=f"px_ref_{ticker_escolhido}")
         mostrar_operacoes(operacoes_processadas, ticker_escolhido, px_ref)
-
 
 
 
