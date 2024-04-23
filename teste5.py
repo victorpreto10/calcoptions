d3b569fa1996564b016dc906e185e2c9fe3269b9
victorpreto10 <150963273+victorpreto10@users.noreply.github.com>
2024-04-23 09:57:36 -0300

    Update teste5.py

--- teste5.py
+++ teste5.py
@@ -241,7 +241,7 @@ def calcular_opcao(tipo_opcao, metodo_solucao, preco_subjacente, preco_exercicio
 st.sidebar.title("Menu de Navegação")
 opcao = st.sidebar.radio(
     "Escolha uma opção:",
-    ('Home', 'Calcular Volatilidade Implícita', 'Calcular Preço de Opções', 'Leitor Recap Kap','Pegar Volatilidade Histórica','Niveis Kapitalo','Pegar Open Interest', 'Gerar Excel','Spreads Arb','Planilha SPX' 
+    ('Home','Spreads Arb','Niveis Kapitalo', 'Leitor Recap Kap','Planilha SPX','Pegar Volatilidade Histórica','Pegar Open Interest', 'Gerar Excel','Calcular Preço de Opções','Calcular Volatilidade Implícita' 
 ))
 if opcao == 'Home':
     st.image('trading.jpg', use_column_width=True)  # Coloque o caminho da sua imagem
