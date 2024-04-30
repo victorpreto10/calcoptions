912ca4f5f73c74ab15ffa53b0c3f39d2c67a2ab1
victorpreto10 <150963273+victorpreto10@users.noreply.github.com>
2024-04-30 12:53:09 -0300

    Update teste5.py

--- teste5.py
+++ teste5.py
@@ -854,10 +854,6 @@ elif opcao == "XML Opção":
             st.dataframe(st.session_state['options_df'], height=300)
             st.text_area("XML to Copy:", "\n".join(st.session_state['options_df']['XML']), height=100)
 
-    with st.expander("Options Confirmations"):
-        st.dataframe(new_data, height=300)
-            
-
     if st.button("Clear Data"):
         st.session_state['options_df'] = pd.DataFrame(columns=[
             "Action", "Ticker", "Date", "Quantity", "Price", "Option Type", "Strike Price", "Commission", "XML"
