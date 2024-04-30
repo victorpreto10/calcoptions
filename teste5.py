86ab7195d9538ee8fb198f3f83de35b1bd80b00d
victorpreto10 <150963273+victorpreto10@users.noreply.github.com>
2024-04-30 10:39:53 -0300

    Update teste5.py

--- teste5.py
+++ teste5.py
@@ -847,7 +847,7 @@ elif opcao == "XML Opção":
     with st.expander("Options Dashboard"):
         if not st.session_state['options_df'].empty:
             st.dataframe(st.session_state['options_df'])
-            st.text_area("XML to Copy:", "\n".join(st.session_state['options_df']['XML']), height=100)
+            st.text_area("XML to Copy:", "\n".join(st.session_state['options_df']['XML']), height=300)
 
     # Botão para limpar o DataFrame
     if st.button("Clear Data"):
